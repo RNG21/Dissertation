3629c6ca0b42b92122938ceeb2ff20a50ffec764
RNG21 <frankytsang921@gmail.com>
2025-04-27 13:15:00 +0100

    builder init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home';
+import CommandBuilder from './builder/builder';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/command_builder" element={<CommandBuilder />} />
             </Routes>
         </Router>
     );
--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -4,7 +4,7 @@ import modules from "./modules.json";
 import { Module } from './types';
 
 const ModuleCard = ({ title, description, icon }: Module) => (
-    <div className="bg-[#2e2e38] text-white p-4 rounded-md w-64 h-52 flex flex-col justify-between">
+    <div className="bg-[#2e2e38] hover:bg-[#3b3b47] transition-colors duration-200 text-white p-4 rounded-md w-64 h-52 flex flex-col justify-between">
         <div className="flex items-center justify-center text-4xl">{icon}</div>
         <div className="text-lg font-bold mt-2">{title}</div>
         <div className="text-sm text-gray-300 mt-1">{description}</div>
@@ -13,13 +13,15 @@ const ModuleCard = ({ title, description, icon }: Module) => (
 
 const ModulesGrid = () => {
     return (
-        <div className="min-h-screen bg-[#1e1e26] py-10 px-6">
-            <h1 className="text-2xl font-bold text-white mb-6">Featured &amp; Favourited</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="py-10 px-6">
+            <h1 className="text-2xl font-bold text-black dark:text-white mb-6">Quick Access</h1>
+            <div className="flex flex-wrap gap-6">
                 {modules.map((module: Module, index) => (
-                <div key={index} className="relative">
-                    <ModuleCard {...module}/>
-                </div>
+                    <a href={module.url} key={index}>
+                        <div className="relative cursor-pointer">
+                            <ModuleCard {...module} />
+                        </div>
+                    </a>
                 ))}
             </div>
         </div>
@@ -27,7 +29,7 @@ const ModulesGrid = () => {
 };
 
 function Home() {
-    return <Base mainContent={ModulesGrid} />;
+    return <Base pageName={"Dashboard"} mainContent={ModulesGrid} />;
 }
 
 export default Home;
